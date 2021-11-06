95bd0bb930b36fd55b02bfc073e98408c85554d6
piotrdobrzynski <50482622+piotrdobrzynski@users.noreply.github.com>
2021-11-06 12:38:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,3 +23,19 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+// 1. Nowa metoda w SW
+self.addEventListener('push', (event) => {
+    // 2. Sprawdź wiadomośc z serwera i sparsuj do tekstu
+    console.log('Otrzymałem nowe dane z serwera:', event.data.text())
+    // 3. Stwórz tytuł i treśc notyfikacji. Uzyj danych z serwera
+    const message = {
+        data: event.data.text()
+    }
+    const title = 'Niesamowita sprawa!';
+
+    // 4. Stwórz notyfikację
+    const promiseChain = self.registration.showNotification(title, message);
+    // 5. Wywołaj notyfikację
+    event.waitUntil(promiseChain);
+})
